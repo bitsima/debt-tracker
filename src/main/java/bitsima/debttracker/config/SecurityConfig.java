46e77a27f681edf39a545e485fa849e38061de40
bitsima <betulse42@gmail.com>
2023-09-12 15:12:07 +0300

    feat: add security config template

---
+++ src/main/java/bitsima/debttracker/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package bitsima.debttracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/ignore1", "/ignore2");
+    }
+}
