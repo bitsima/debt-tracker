faf7fe2e33036e8bf7ebd17f2cf32ae5fe37638a
bitsima <betulse42@gmail.com>
2023-09-13 11:40:48 +0300

    feat: add custom method 'findByDebtType' to the repository

--- src/main/java/bitsima/debttracker/repository/DebtRepository.java
+++ src/main/java/bitsima/debttracker/repository/DebtRepository.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import bitsima.debttracker.model.Debt;
+import bitsima.debttracker.model.DebtType;
+
+import java.util.List;
 
 public interface DebtRepository extends JpaRepository<Debt, UUID> {
-
+    List<Debt> findByDebtType(DebtType debtType);
 }
