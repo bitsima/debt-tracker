cfb38fd2e760c7c2b4dc11abb28c6d090f3c8a5c
bitsima <betulse42@gmail.com>
2023-09-19 10:56:33 +0300

    feat: create the security config with url path matching

--- src/main/java/bitsima/debttracker/config/SecurityConfig.java
+++ src/main/java/bitsima/debttracker/config/SecurityConfig.java
@@ -2,13 +2,21 @@ package bitsima.debttracker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/ignore1", "/ignore2");
+    public SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange((authorize) -> authorize
+                        .pathMatchers("/api/v1/auth/**").permitAll()
+                        .pathMatchers("/api/v1/taxpayer/**").hasRole("TAXPAYER")
+                        .pathMatchers("/api/v1/municipality/**").hasRole("MUNICIPALITY_ADMIN")
+                        .pathMatchers("/api/v1/system/**").hasRole("SYSTEM_ADMIN")
+                        .anyExchange().denyAll());
+        return http.build();
     }
 }
